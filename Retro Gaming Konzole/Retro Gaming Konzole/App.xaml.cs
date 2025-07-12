04b688e9e95206c475b682056a8261fb5f804d5f
Dimitrije Stanković <diki.zajecar@gmail.com>
2025-07-12 16:10:39 +0200

    Created WPF Solution

---
+++ Retro Gaming Konzole/Retro Gaming Konzole/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Retro_Gaming_Konzole
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
